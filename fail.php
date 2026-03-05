bc0ea913bc4f2b42ffa56c07fe1e27a4f2e3cc6f
dinasamuel211-del <dinasamuel211@gmail.com>
2026-03-05 00:19:51 -0800

    Add files via upload

---
+++ fail.php
@@ -0,0 +1,8 @@
+<?php
+// This file intentionally contains a syntax error
+// Do NOT fix this file - the error is required by the assignment
+// to verify that PHP error display is working in your server
+
+$x = "Hello World"
+print $x;
+?>
